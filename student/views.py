f87738c9ca872a0292504ad1f1f729e6f5a23396
Eli Sheldon <eli.sheldon@gmail.com>
2019-10-10 13:40:41 -0700

    fix migrations issue and missing public link

--- student/views.py
+++ student/views.py
@@ -28,10 +28,12 @@ def portfolio(request):
     student = Student.objects.get(user = request.user)
     items = Item.objects.filter(student = student)
     public = student.pf_public
+    public_link = 'https://showcaseedu.com/student/view/' + request.user.username
     context = {
         'items': items,
         'name': request.user.first_name,
-        'public': public
+        'public': public,
+        'link': public_link,
     }
     return render(request, 'student/portfolio.html', context)
 
