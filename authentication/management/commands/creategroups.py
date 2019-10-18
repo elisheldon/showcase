17da6b41f6cfb2c37d766ef684f4ce2132bf7779
Eli Sheldon <eli.sheldon@gmail.com>
2019-10-18 08:08:02 -0700

    minor bugfixes

--- authentication/management/commands/creategroups.py
+++ authentication/management/commands/creategroups.py
@@ -6,5 +6,5 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not Group.objects.filter(name='students').exists():
             Group.objects.create(name='students')
-        if not Group.objects.filter(name='teachers').exists():
-            Group.objects.create(name='teachers')
+        if not Group.objects.filter(name='staff').exists():
+            Group.objects.create(name='staff')
