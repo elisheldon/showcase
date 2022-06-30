8a794ef3a15c874517ff9915c2cdfb1369835d0f
Eli Sheldon <eli.sheldon@allhere.com>
2022-06-30 15:15:00 -0700

    Fix gunicorn config

--- config/gunicorn/dev.py
+++ config/gunicorn/dev.py
@@ -1,7 +1,7 @@
 """Gunicorn *development* config file"""
 
 # Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
-wsgi_app = "project.wsgi:application"
+wsgi_app = "portfolio.wsgi:application"
 # The granularity of Error log outputs
 loglevel = "debug"
 # The number of worker processes for handling requests
