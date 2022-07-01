8aae4965d58cc133a8db9e4c5fcf5063e29c0d93
Eli Sheldon <eli.sheldon@allhere.com>
2022-07-01 13:41:09 -0700

    Updated settings again

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -167,6 +167,19 @@ SOCIAL_AUTH_MICROSOFT_GRAPH_SECRET = os.environ['AZURE_SECRET']
 
 SOCIAL_AUTH_UUID_LENGTH = 3
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
 # Logging Configuration
 
 # Clear prev config
--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -10,16 +10,3 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_HSTS_SECONDS = 2592000
 SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-
-SOCIAL_AUTH_PIPLEINE = (
-    'social_core.pipeline.social_auth.social_details',
-    'social_core.pipeline.social_auth.social_uid',
-    'social_core.pipeline.social_auth.auth_allowed',
-    'social_core.pipeline.social_auth.social_user',
-    'social_core.pipeline.user.get_username',
-    'social_core.pipeline.social_auth.associate_by_email',
-    'social_core.pipeline.user.create_user',
-    'social_core.pipeline.social_auth.associate_user',
-    'social_core.pipeline.social_auth.load_extra_data',
-    'social_core.pipeline.user.user_details',
-)
